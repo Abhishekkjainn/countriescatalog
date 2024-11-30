279c96bbd773753f8f4cb15c44d865ae0eadc4ff
abhishekkjainn <jainabhishek1904@gmail.com>
2024-12-01 02:19:46 +0530

    Added first page

--- src/App.jsx
+++ src/App.jsx
@@ -74,8 +74,36 @@ function App() {
         </div>
       </div>
       <div className="page1">
-        <div className="section1"></div>
-        <div className="section2"></div>
+        <div className="section1">
+          <div className="heading">
+            Empowering Your Apps with Global Knowledge.
+          </div>
+          <div className="subheadline">
+            Effortlessly access accurate, country-specific data to power your
+            apps. From population stats to currency details, our free API
+            delivers it all—fast, reliable, and always up-to-date.
+          </div>
+          <div className="buttonssection">
+            <div className="primarybutton">
+              API Docs{' '}
+              <img src="/arrow.png" alt="arrow" className="arrowdown" />
+            </div>
+            <div className="secondarybutton">
+              Contribute
+              <img src="/arrow.png" alt="arrow" className="arrowdown" />
+            </div>
+          </div>
+        </div>
+        <div className="section2">
+          <Player
+            src="/globe.json"
+            className="player"
+            loop
+            autoplay
+            // background="black"
+            style={{ width: '100%' }}
+          />
+        </div>
       </div>
     </>
   );
